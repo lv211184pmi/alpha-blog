cbc1dcd3ad7579e7c821427c7467eee81ec2dbcd
MP <lv211184pmi@gmail.com>
2016-01-25 21:01:04 +0000

    selector form at articles controller and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
   
+before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+  
   def index
     @article = Article.all
   end
@@ -10,7 +13,6 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
   
 
@@ -26,7 +28,6 @@ class ArticlesController < ApplicationController
   
     
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Your article was successfully updated"
       redirect_to article_path(@article)
@@ -37,12 +38,10 @@ class ArticlesController < ApplicationController
   
   
   def show
-    @article = Article.find(params[:id])
   end
 
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "This article was successfully deleeted"
     redirect_to articles_path
@@ -50,6 +49,10 @@ class ArticlesController < ApplicationController
 
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+  
     def article_params
       params.require(:article).permit(:title, :description)
     end
