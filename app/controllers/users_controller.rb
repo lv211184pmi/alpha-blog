85af966d206d0a8e3d8c74c5076ba50d7ab3f93d
lv211184pmi1 <smth_else@mail.ru>
2016-03-06 16:51:12 +0000

    Restrictions for user's session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:edit, :update, :show]
+  before_action :require_user_profile, only: [:edit, :update]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -19,11 +21,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was successfully updated"
       redirect_to articles_path
@@ -33,12 +33,23 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
   
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+  
+  def require_user_profile
+    if current_user != @user
+      flash[:danger] = "You can edit or update only your profile"
+      redirect_to users_path
+    end
+  end
+    
 end
